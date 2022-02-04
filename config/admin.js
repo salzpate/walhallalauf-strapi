5b26e6e59d13368a5e8c773d59785648fe82d32c
Ronny Krammer <ronny.krammer@gwvs.de>
2022-02-04 23:57:13 +0100

    Admin

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,11 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
   auth: {
+    options: {
+      expiresIn: "365d",
+    },
     secret: env('ADMIN_JWT_SECRET'),
   },
 });
